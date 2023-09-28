5006cf2d0120133adb73e5fbc8d6e900c7aa97cf
Zombie <73972020+Shiro4k@users.noreply.github.com>
2023-09-28 21:43:26 +0800

    add Two Dz

---
+++ Task 64/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+    N = 5 -> "5, 4, 3, 2, 1"
+    N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+ */
+
+int SetNumber(string message)
+{
+    System.Console.Write($"Enter number {message}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void GetShislo(int N, int Count = 1)
+{
+    if (Count == N)
+    {
+        Console.WriteLine(N);
+        return;
+    }
+    Console.Write($"{N--}, ");
+    GetShislo(N, Count);
+}
+
+Console.Clear();
+int N = SetNumber("Введите число N");
+GetShislo(N);
---
+++ Task 66/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+    M = 1; N = 15 -> 120
+    M = 4; N = 8. -> 30
+ */
+
+int GetNumber(string message)
+{
+    Console.Write($"{message}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int SumNumbers(int N, int M)
+{
+    int Sum = 0;
+    if (M == N)
+    {
+        return 0; 
+    }
+    else
+    {
+        M++;
+        Sum = M + SumNumbers(N, M);
+        return Sum;
+    }
+}
+
+Console.Clear();
+int M = GetNumber("Введите число от M: ");
+int N = GetNumber("Введите число до N: ");
+int sum = SumNumbers(N, M - 1);
+Console.WriteLine(sum);
+
